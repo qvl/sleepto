2455a07c1c4a4f4d4bee708f9c8be77c9f4c7b42
Jorin Vogel <hi@jorin.me>
2017-01-04 15:43:59 +0100

    flags.Weekdaylist

---
+++ flags/weekdaylist.go
@@ -0,0 +1,59 @@
+package flags
+
+import (
+	"flag"
+	"fmt"
+	"strings"
+	"time"
+)
+
+var strDays = map[string]time.Weekday{
+	"mo": time.Monday,
+	"tu": time.Tuesday,
+	"we": time.Wednesday,
+	"th": time.Thursday,
+	"fr": time.Friday,
+	"sa": time.Saturday,
+	"su": time.Sunday,
+}
+
+type weekdaylist struct {
+	list []time.Weekday
+}
+
+func (l *weekdaylist) String() string {
+	s := make([]string, len(l.list))
+	for i := range l.list {
+		s[i] = dayToString(l.list[i])
+	}
+	return strings.Join(s, ",")
+}
+
+func dayToString(d time.Weekday) string {
+	for k, v := range strDays {
+		if v == d {
+			return k
+		}
+	}
+	return ""
+}
+
+func (l *weekdaylist) Set(s string) error {
+	days := strings.Split(s, ",")
+	for i, d := range days {
+		x, ok := strDays[d]
+		if !ok {
+			return fmt.Errorf("invalid day at index %d: %s", i, d)
+		}
+		l.list = append(l.list, x)
+	}
+	return nil
+}
+
+// Weekdaylist defines a flag for a comma-separated list of week-days.
+// Valid values are mo, tu, we, th, fr, sa, su.
+func Weekdaylist(name, usage string) []time.Weekday {
+	l := &weekdaylist{}
+	flag.Var(l, name, usage)
+	return l.list
+}
---
+++ flags/weekdaylist_test.go
@@ -0,0 +1,57 @@
+package flags
+
+import (
+	"testing"
+	"time"
+)
+
+func TestWeekdaylist(t *testing.T) {
+	tests := []struct {
+		text    string
+		parsed  []time.Weekday
+		invalid bool
+	}{
+		{text: "tu,th,fr", parsed: []time.Weekday{time.Tuesday, time.Thursday, time.Friday}},
+		{text: "sa,su", parsed: []time.Weekday{time.Saturday, time.Sunday}},
+		{text: "mo", parsed: []time.Weekday{time.Monday}},
+		{text: "mo,tu,", invalid: true},
+		{text: "1,2", invalid: true},
+		{text: "0", invalid: true},
+		{text: "", invalid: true},
+		{text: "hello", invalid: true},
+	}
+
+	for i, tt := range tests {
+		l := weekdaylist{}
+		if err := l.Set(tt.text); err != nil {
+			if !tt.invalid {
+				t.Errorf("parsing %s failed unexpectedly: %v", tt.text, err)
+			}
+			continue
+		}
+		if tt.invalid {
+			t.Errorf("parsing %s should have failed", tt.text)
+			continue
+		}
+		if !equalWeekday(l.list, tt.parsed) {
+			t.Errorf(`
+%d.
+Input:    %s
+Expected: %v
+Got       %v`, i, tt.text, tt.parsed, l.list)
+		}
+	}
+
+}
+
+func equalWeekday(a []time.Weekday, b []time.Weekday) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
